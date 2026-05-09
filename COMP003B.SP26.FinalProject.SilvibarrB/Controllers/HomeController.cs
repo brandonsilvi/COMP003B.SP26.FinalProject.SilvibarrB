74ccbc510b1aebf0709d09e8acec5cb9a691af5c
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-05-08 22:44:23 -0700

    Test route added to HomeController.cs

--- COMP003B.SP26.FinalProject.SilvibarrB/Controllers/HomeController.cs
+++ COMP003B.SP26.FinalProject.SilvibarrB/Controllers/HomeController.cs
@@ -21,4 +21,10 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    [Route("Home/Test/{message?}")]
+    public IActionResult Test(string? message)
+    {
+        return Content($"Message: {message}");
+    }
 }
